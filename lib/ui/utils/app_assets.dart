3f43892da8b1b65e8e8a3fe3712ece8b3fcc314b
EbrahimTarek02 <ebrahimtarek2002@gmail.com>
2023-10-22 16:36:05 +0300

    Initial Commit.

---
+++ lib/ui/utils/app_assets.dart
@@ -0,0 +1,12 @@
+abstract class AppAssets {
+  static const String imagesPath = 'assets/images/';
+
+  static const String background = '${imagesPath}background.png';
+  static const String ball = '${imagesPath}ball.png';
+  static const String business = '${imagesPath}business.png';
+  static const String environment = '${imagesPath}environment.png';
+  static const String health = '${imagesPath}health.png';
+  static const String politics = '${imagesPath}politics.png';
+  static const String science = '${imagesPath}science.png';
+  static const String splash = '${imagesPath}splash.png';
+}
---
+++ lib/ui/utils/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color primary = Color(0xff39A552);
+  static const Color accent = Colors.white;
+  static const Color red = Color(0xffC91C22);
+  static const Color navy = Color(0xff003E90);
+  static const Color pink = Color(0xffED1E79);
+  static const Color green = Color(0xffCF7E48);
+  static const Color blue = Color(0xff4882CF);
+  static const Color yellow = Color(0xffF2D352);
+  static const Color textColor = Color(0xff4F5A69);
+}
